a98c957fa0cc7c1873abb5527ac2f49cb91212f1
{Akshar1199} <{aksharparekh1199@gmail.com}>
2023-10-27 19:42:34 +0530

    commit

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -55,8 +55,6 @@ namespace Expense_Tracker_System.Controllers
         }
 
         // POST: Transaction/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("TransactionId,CategoryId,Amount,Description,Date")] Transaction transaction)
